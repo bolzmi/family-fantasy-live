5267b1898154acc86eb3ac6d677a187a0816a6bb
bolzmi <45827990+bolzmi@users.noreply.github.com>
2026-09-13 13:36:23 -0500

    Configure Google Cast receiver app ID

--- cast-config.js
+++ cast-config.js
@@ -1,2 +1,2 @@
-// Google Cast application ID. Leave blank until the Custom Web Receiver is registered.
-window.BOLZ_CAST_APP_ID = '';
+// Google Cast Custom Web Receiver application ID.
+window.BOLZ_CAST_APP_ID = '86587653';
